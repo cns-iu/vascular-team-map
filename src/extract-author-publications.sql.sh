2e55e0e341f692180afce584ac0cd12aa2157ae7
Bruce Herr II <bherr@indiana.edu>
2019-04-19 12:01:49 -0400

    Expanding the search to start with email addresses, and then find connected dais, orcid, ResearcherIDs, and display_names to find more documents. Data is saved to a table for inspection afterwards.

--- src/extract-author-publications.sql.sh
+++ src/extract-author-publications.sql.sh
@@ -14,16 +14,77 @@ psql << EOF
 
   \COPY authors_for_search FROM '${1}' WITH Delimiter ',' CSV HEADER Encoding 'SQL-ASCII'
 
-  -- CREATE INDEX wos_summary_names_email_addr_email_search_idx ON wos_summary_names_email_addr(lower(email_addr), id);
+  -- CREATE INDEX wos_summary_names_email_addr_email_search_idx ON wos_summary_names_email_addr(lower(email_addr), id, name_id);
   -- CREATE INDEX wos_summary_names_lastn_search_idx ON wos_summary_names(lower(last_name), first_name, id);
 
   DROP TABLE IF EXISTS matched_author_publications;
-
   CREATE TABLE matched_author_publications AS (
-      SELECT K.id, 'email'::text AS matched_by
+    WITH email_authors AS (
+      SELECT K.id, K.name_id, 'email'::text AS matched_by
       FROM wos_summary_names_email_addr AS K, authors_for_search AS A
       WHERE lower(K.email_addr) = lower(A.email_address)
+    ),
+    dais_for_search AS (
+      SELECT DISTINCT dais_id
+      FROM wos_summary_names AS S, email_authors AS A
+      WHERE S.id = A.id AND S.name_id = A.name_id AND dais_id IS NOT NULL
+    ),
+    orcid_for_search AS (
+      SELECT DISTINCT coalesce(orcid_id, orcid_id_tr) AS orcid_id
+      FROM wos_summary_names AS S, email_authors AS A
+      WHERE S.id = A.id AND S.name_id = A.name_id AND coalesce(orcid_id, orcid_id_tr) IS NOT NULL
+    ),
+    researcherid_for_search AS (
+      SELECT DISTINCT coalesce(r_id, r_id_tr) AS r_id
+      FROM wos_summary_names AS S, email_authors AS A
+      WHERE S.id = A.id AND S.name_id = A.name_id AND coalesce(r_id, r_id_tr) IS NOT NULL
+    ),
+    display_name_for_search AS (
+      SELECT DISTINCT display_name
+      FROM wos_summary_names AS S, email_authors AS A
+      WHERE S.id = A.id AND S.name_id = A.name_id AND display_name IS NOT NULL
+    ),
+
+    dais_authors AS (
+      SELECT S.id, S.name_id, 'dais-via-email'::text as matched_by
+      FROM wos_summary_names AS S, dais_for_search AS A
+      WHERE S.dais_id = A.dais_id
+    ),
+    orcid_authors AS (
+      SELECT S.id, S.name_id, 'orcid-via-email'::text as matched_by
+      FROM wos_summary_names AS S, orcid_for_search AS A
+      WHERE S.orcid_id = A.orcid_id OR S.orcid_id_tr = A.orcid_id
+    ),
+    researcherid_authors AS (
+      SELECT S.id, S.name_id, 'researcherid-via-email'::text as matched_by
+      FROM wos_summary_names AS S, researcherid_for_search AS A
+      WHERE S.r_id = A.r_id OR S.r_id_tr = A.r_id
+    ),
+    display_name_authors AS (
+      SELECT S.id, S.name_id, 'display-name-via-email'::text as matched_by
+      FROM wos_summary_names AS S, display_name_for_search AS A
+      WHERE S.display_name = A.display_name
+    )
+    SELECT id, array_to_string(array_agg(matched_by), '|') AS matched_by
+    FROM (
+      SELECT id, matched_by FROM email_authors
+      UNION
+      SELECT id, matched_by FROM dais_authors
+      UNION
+      SELECT id, matched_by FROM orcid_authors
+      UNION
+      SELECT id, matched_by FROM researcherid_authors
+      UNION
+      SELECT id, matched_by FROM display_name_authors
+    ) AS A
+    GROUP BY id
   );
+
+  -- CREATE TABLE matched_author_publications AS (
+  --     SELECT K.id, 'email'::text AS matched_by
+  --     FROM wos_summary_names_email_addr AS K, authors_for_search AS A
+  --     WHERE lower(K.email_addr) = lower(A.email_address)
+  -- );
 
   -- More complicated search if we are doing multiple search criteria to find publications
   -- CREATE TABLE matched_author_publications AS (
